7ca2f4a61a0d699712d34cb5bffc1a589d0d3940
Antoinesrvt <antoine.srvt@gmail.com>
2022-09-23 15:44:33 +0200

    add components on front

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import {Navbar, Footer } from '../components';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  <div>
+    <Navbar/>
+    <Component {...pageProps} />
+    <Footer />
+    <Script src="https://kit.fontawesome.com/6e73e83c37.js" crossOrigin="anonymous"></Script>
+  </div>
 }
 
 export default MyApp
